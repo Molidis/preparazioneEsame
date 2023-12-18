4978e1fcf9818471d6c7412f377434fd1c62b5e4
Molidis <98560872+Molidis@users.noreply.github.com>
2023-12-18 19:22:08 +0100

    first commit

---
+++ pariDispari/pariDispari.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func isEven(n int) bool {
+	return n%2 == 0
+}
+
+func main() {
+
+	var previousWasEven bool
+	primo, err := strconv.Atoi(os.Args[1])
+
+	if len(os.Args) == 1 {
+		fmt.Println("nessun valore in ingresso")
+		return
+	}
+
+	if isEven(primo) {
+		previousWasEven = true
+	} else {
+		previousWasEven = false
+	}
+
+	if err != nil {
+		fmt.Println("elemento in posizione", 1, "non valido")
+		return
+	}
+
+	for i, v := range os.Args[2:] {
+		n, err := strconv.Atoi(v)
+		if err != nil {
+			fmt.Println("elemento in posizione", i+2, "non valido")
+			return
+		}
+		if (isEven(n) && previousWasEven) || (!isEven(n) && !previousWasEven) {
+			fmt.Println("elemento in posizione", i+2, "non valido")
+			return
+		}
+		if isEven(n) {
+			previousWasEven = true
+		} else {
+			previousWasEven = false
+		}
+	}
+
+	fmt.Println("sequenza valida")
+
+}
