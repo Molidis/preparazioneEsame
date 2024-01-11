fd338ff51abcf17c3aaa47796ef4b03c292ec14d
Molidis <98560872+Molidis@users.noreply.github.com>
2024-01-11 18:28:51 +0100

    Updated

--- dispensa/dispensa_test.go
+++ dispensa/dispensa_test.go
@@ -1,5 +1,4 @@
 /*
-
 Dispensa
 --------
 
@@ -9,21 +8,27 @@ Il programma leggerà da un file CSV (Comma Separated Values) un "diario" degli 
 
 Il programma dovrà essere dotato delle seguenti:
 
-- una funzione AggiornaDispensa(dispensa map[string]float64, filename string) bool
-	che aggiorna una dispensa prendendo i dati da un file CSV il cui nome è passato come parametro.
-	In particolare il file conterrà gli acquisti nel formato "approv,<prodotto>,<peso>" e i consumi nel formato "uso,<prodotto>,<peso>", dove le quantità (pesi) sono sempre espresse in Kg. (Non vanno fatti controlli sul formato dei dati, si può assumere che siano sempre esattamente come nel file di esempio).
-	Se il file non esiste o non è leggibile, la funzione deve terminare subito restituendo 'false'.
-	Nel caso in cui un prodotto dovesse "andare in negativo"  durante l'aggiornamento della dispensa, la funzione dovrà terminare subito e restituire 'false', mentre restituirà 'true' in caso contrario.
+  - una funzione AggiornaDispensa(dispensa map[string]float64, filename string) bool
+    che aggiorna una dispensa prendendo i dati da un file CSV il cui nome è passato come parametro.
+    In particolare il file conterrà gli acquisti nel formato "approv,<prodotto>,<peso>" e i consumi nel
+    formato "uso,<prodotto>,<peso>", dove le quantità (pesi) sono sempre espresse in Kg.
+    (Non vanno fatti controlli sul formato dei dati, si può assumere che siano sempre esattamente come nel file di esempio).
+    Se il file non esiste o non è leggibile, la funzione deve terminare subito restituendo 'false'.
+    Nel caso in cui un prodotto dovesse "andare in negativo"
+    durante l'aggiornamento della dispensa, la funzione dovrà terminare subito e restituire 'false', mentre restituirà
+    'true' in caso contrario.
 
-- una funzione Rimanenza(dispensa map[string]float64, alimento string) float64
-	che, data una dispensa opportunamente popolata di provviste, restituisce la rimanenza in Kg dell'alimento passato come parametro; restituisce 0 se il prodotto non è presente in lista.
+  - una funzione Rimanenza(dispensa map[string]float64, alimento string) float64
+    che, data una dispensa opportunamente popolata di provviste, restituisce la rimanenza in Kg dell'alimento passato
+    come parametro; restituisce 0 se il prodotto non è presente in lista.
 
-- una funzione main()
-	che legge ed elabora un file in formato CSV che contiene i dati relativi ad acquisti e consumi di una dispensa.
-	Il nome del file è passato come primo argomento sulla linea di comando.
-	In caso di presenza di argomenti oltre al nome del file, il programma stampa poi la rimanenza degli alimenti passati come argomenti successivi.
-	In caso di mancanza di argomenti oltre al nome del file, il programma deve stampare solo "file corretto" se nessun prodotto è andato in negativo, e "file non corretto" in caso contrario.
-
+  - una funzione main()
+    che legge ed elabora un file in formato CSV che contiene i dati relativi ad acquisti e consumi di una dispensa.
+    Il nome del file è passato come primo argomento sulla linea di comando.
+    In caso di presenza di argomenti oltre al nome del file, il programma stampa poi la rimanenza degli alimenti passati come
+    argomenti successivi.
+    In caso di mancanza di argomenti oltre al nome del file, il programma deve stampare solo "file corretto" se nessun prodotto
+    è andato in negativo, e "file non corretto" in caso contrario.
 
 Esempi di esecuzione
 ---------------------
@@ -38,8 +43,6 @@ file non corretto
 
 $ ./dispensa dispensa.csv
 file corretto
-
-
 */
 package main
 
--- numeriAbbondanti-/numeriAbbondanti.go
+++ numeriAbbondanti-/numeriAbbondanti.go
@@ -5,25 +5,36 @@ import (
 )
 
 func Abbondante(n int) bool {
+
 	if n <= 0 {
 		return false
 	}
+
 	var somma int = 0
+
 	for i := 1; i < n; i++ {
+
 		if n%i == 0 {
 			somma += i
 		}
+
 	}
+
 	return somma > n
+
 }
 
 func main() {
 	var n, j int
+
 	fmt.Scan(&n)
+
 	for i := 1; j < n; i++ {
+
 		if Abbondante(i) {
 			fmt.Println(i)
 			j++
 		}
+
 	}
 }
