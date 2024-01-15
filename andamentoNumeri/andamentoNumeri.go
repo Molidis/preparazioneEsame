faf845359a1c9bbc2106b5098159faf2d503285f
Molidis <98560872+Molidis@users.noreply.github.com>
2024-01-15 11:10:36 +0100

    Update andamentoNumeri.go

--- andamentoNumeri/andamentoNumeri.go
+++ andamentoNumeri/andamentoNumeri.go
@@ -11,17 +11,17 @@ func main() {
 	var numero int
 
 	scanner := bufio.NewScanner(os.Stdin)
+
 	for scanner.Scan() {
 		fmt.Sscanf(scanner.Text(), "%d", &numero)
-		if primo != 0 {
-			if numero > primo {
-				fmt.Print("+")
-			} else if numero == primo {
-				fmt.Print("=")
-			} else {
-				fmt.Print("-")
-			}
+		if numero > primo {
+			fmt.Print("+")
+		} else if numero == primo {
+			fmt.Print("=")
+		} else {
+			fmt.Print("-")
 		}
+
 		primo = numero
 		if scanner.Err() != nil {
 			fmt.Println(scanner.Err())
